e38f55e96686df19208340829fecd3dc470edd8b
Aayush panchal <codewithaayushofficial@gmail.com>
2026-06-28 16:18:48 +0530

    Time: 1 ms (75.08%), Space: 43.1 MB (17.39%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,17 +1,7 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder ans = new StringBuilder();
-        for (int i = 0; i < command.length(); i++) {
-            if (command.charAt(i) == '(' && command.charAt(i+1) == 'a') {
-                ans.append("al");
-                i += 3;
-            }   else if (command.charAt(i) == '(' && command.charAt(i+1) == ')') {
-                ans.append("o");
-                i++;
-            }   else {
-                ans.append("G");
-            }
-        }
-        return ans.toString();
+        String ans = command.replace("()", "o") ;
+        String result = ans.replace("(al)", "al") ;
+        return result ;
     }
 }
