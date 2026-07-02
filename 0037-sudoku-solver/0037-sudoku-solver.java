f132ede2964444ec31d7e61887e004c33d5394f6
Aayush panchal <codewithaayushofficial@gmail.com>
2026-07-02 22:01:39 +0530

    Time: 224 ms (60.72%), Space: 42.9 MB (34.18%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,98 +1,71 @@
+// Gemini solution to see the time and space
+
+
 class Solution {
     public void solveSudoku(char[][] board) {
-        int[][] arr = new int[9][9];
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    arr[i][j] = 0;
-                }   else {
-                    arr[i][j] = board[i][j] - '0';
-                }
-            }
-        }
-
-        solve(arr);
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                board[i][j] = (char)(arr[i][j] + '0');
-            }
-        }
+        solve(board);
     }
 
-    static boolean solve(int[][] board) {
-            int n = board.length;
-            int row = -1;
-            int col = -1;
+    private boolean solve(char[][] board) {
+        int n = board.length;
+        int row = -1;
+        int col = -1;
+        boolean emptyLeft = true;
 
-            boolean emptyLeft = true;
-
-            // this is how we are replacing the r,c from arguments
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (board[i][j] == 0) {
-                        row = i;
-                        col = j;
-                        emptyLeft = false;
-                        break;
-                    }
-                }
-                // if you found some empty element in row, then break
-                if (emptyLeft == false) {
+        // Find the first empty cell denoted by '.'
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == '.') {
+                    row = i;
+                    col = j;
+                    emptyLeft = false;
                     break;
                 }
             }
-
-            if (emptyLeft == true) {
-                return true;
-                // soduko is solved
+            if (!emptyLeft) {
+                break;
             }
+        }
 
-            // backtrack
-            for (int number = 1; number <= 9; number++) {
-                if (isSafe(board, row, col, number)) {
-                    board[row][col] = number;
-                    if (solve(board)) {
-                        // found the answer
-                        return true;
-                    } else {
-                        // backtrack
-                        board[row][col] = 0;
-                    }
+        // If no empty cell is left, Sudoku is solved
+        if (emptyLeft) {
+            return true;
+        }
+
+        // Backtrack using characters '1' through '9'
+        for (char val = '1'; val <= '9'; val++) {
+            if (isSafe(board, row, col, val)) {
+                board[row][col] = val;
+                
+                if (solve(board)) {
+                    return true;
+                } else {
+                    // Undo the choice (backtrack)
+                    board[row][col] = '.';
                 }
             }
-            return false;
+        }
+        return false;
+    }
+
+    private boolean isSafe(char[][] board, int row, int col, char val) {
+        // Check row and column in a single loop
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == val) return false;
+            if (board[i][col] == val) return false;
         }
 
-    static boolean isSafe(int[][] board, int row, int col, int num) {
-        // check the row
-        for (int i = 0; i < board.length; i++) {
-            // check if the number is in the row
-            if (board[row][i] == num) {
-                return false;
-            }
-        }
+        // Check the 3x3 sub-grid
+        int rowStart = row - row % 3;
+        int colStart = col - col % 3;
 
-        // check the col
-        for (int[] nums : board) {
-            // check if the number is in the col
-            if (nums[col] == num) {
-                return false;
-            }
-        }
-
-        int sqrt = (int) (Math.sqrt(board.length));
-        int rowStart = row - row % sqrt;
-        int colStart = col - col % sqrt;
-
-        for (int r = rowStart; r < rowStart + sqrt; r++) {
-            for (int c = colStart; c < colStart + sqrt; c++) {
-                if (board[r][c] == num) {
+        for (int r = rowStart; r < rowStart + 3; r++) {
+            for (int c = colStart; c < colStart + 3; c++) {
+                if (board[r][c] == val) {
                     return false;
                 }
             }
         }
         return true;
     }
-
 }
