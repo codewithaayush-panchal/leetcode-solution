7096ffdcc25a434444715052beceeb008c2041c5
Aayush panchal <codewithaayushofficial@gmail.com>
2026-08-09 11:47:01 +0530

    Time: 0 ms (100%), Space: 43.1 MB (16.56%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list-minor-change.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int length = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            length++;
+            temp = temp.next;
+        }
+        ListNode ans = head;
+        for (int i = 0; i < length / 2; i++) {
+                ans = ans.next;
+            }
+        return ans;
+    }
+}
